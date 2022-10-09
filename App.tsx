bd9d3b80bf78d8144f9b26c2e880fea2d9321ef5
Allan Lopez <allanloji@gmail.com>
2022-10-09 15:05:02 -0500

    :sparkles:(styles): add styled components dependency

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import styled from 'styled-components/native';
+
+const Container = styled(View)`
+  flex: 1;
+  background-color: '#fff';
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Container>
       <StatusBar style="auto" />
-    </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
